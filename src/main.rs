20aeedb4894e6526d20d2a806b3a49c151c060af
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2021-06-28 21:05:29 -0700

    get emote from db when sent

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,7 @@ use regex::Regex;
 use sqlx::SqlitePool;
 
 use std::sync::Arc;
-use std::fs::File;
-use std::io::Read;
+use std::fs;
 
 struct Handler;
 struct Db {
@@ -45,7 +44,24 @@ impl HoldsDb for Context {
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
+        lazy_static! {
+            static ref RE = Regex::new("<a?:(.{2,32}):([0-9]{17,19})").unwrap();
+        }
         println!("{}", msg.content);
+        if RE.is_match(&msg.content) {
+            let db = ctx.data.read().await.get::<Db>().unwrap().clone();
+            let mut conn = db.pool.acquire().await.unwrap();
+            let cap = RE.captures_iter(&msg.content).next().unwrap();
+            let emote_id = &cap[2];
+            let emote_name = &cap[1];
+            let id = emote_id.parse::<i64>().unwrap();
+            let emote = sqlx::query!("SELECT * FROM emotes WHERE id=?;", id).fetch_optional(&mut conn).await.unwrap();
+            sqlx::query!("INSERT INTO users VALUES (?, ?);", *msg.author.id.as_u64() as i64 /* ffs sqlite devs */, emote_id).execute(&mut conn).await.unwrap();
+            if emote.is_some() {
+                let emote = emote.unwrap();
+                //sqlx::query!("REPLACE INTO emotes VALUES (?, ?, ?, ?);", emote_id, emote_name, emote.uses + 1, uniq).execute(&mut conn).await.unwrap();
+            }
+        }
     }
     async fn ready(&self, ctx: Context, ready: Ready) {
         println!("client ready");
@@ -55,10 +71,8 @@ impl EventHandler for Handler {
 #[tokio::main]
 async fn main() {
     let db = Db::new().await;
-    let mut f = std::fs::File::open(".bot-token").unwrap();
-    let mut tok: Vec<u8> = vec![];
-    f.read(&mut tok).unwrap();
-    let mut client = Client::builder(String::from_utf8_lossy(&tok[..])).event_handler(Handler).intents(GatewayIntents::non_privileged()).await.expect("client creation error");
+    let frm = serenity::framework::StandardFramework::new();
+    let mut client = Client::builder(fs::read_to_string(".bot-token").unwrap()).event_handler(Handler).intents(GatewayIntents::non_privileged()).framework(frm).await.expect("client creation error");
     let mut data = client.data.write().await;
     data.insert::<Db>(Arc::new(db));
     drop(data);
